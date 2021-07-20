00e08f06edf82583cffdd2d01af151d59cfaa298
Fabricio Catae <fcatae@hotmail.com>
2021-07-20 11:03:47 -0300

    add files

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const WebSocket = require('ws');
+const express = require('express');
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
+const INDEX = '/index.html';
 
-const wss = new WebSocket.Server({ port: PORT });
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
